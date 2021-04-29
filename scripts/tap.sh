e021647dd9e602389f4c1853b01356f1cf95f8b1
mpcarolin <mpcarolin.dev@gmail.com>
2021-04-29 14:44:24 -0700

    Updated with action-specific tap alias

--- scripts/tap.sh
+++ scripts/tap.sh
@@ -5,6 +5,7 @@ export TAPS_DIR=$ACTION_WORKSPACE/keg-hub/taps
 export TAP_BRANCH=${INPUT_TAP_REF##*/}
 export TAP_NAME=$(basename $INPUT_REPOSITORY)
 export TAP_PATH=$TAPS_DIR/$TAP_NAME
+export TAP_ALIAS="client_tap"
 
 clone_tap () {
   echo "==== Cloning Tap with branch $TAP_BRANCH"
@@ -20,12 +21,12 @@ install_tap () {
 link_tap () {
   cd $TAP_PATH
   echo "==== Linking tap"
-  keg tap link
+  keg tap link $TAP_ALIAS
 }
 
 build_push_tap () {
   echo "==== Building and pushing tap"
-  GIT_KEY=$INPUT_TOKEN keg $TAP_NAME build --push --tag $INPUT_TAG --no-cache
+  GIT_KEY=$INPUT_TOKEN keg $TAP_ALIAS build --push --tag $INPUT_TAG --no-cache
 }
 
 clone_tap

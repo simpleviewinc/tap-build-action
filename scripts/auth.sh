3a6f1c25754dc4bfa5f2abad305792f0d1f630d1
mpcarolin <mpcarolin.dev@gmail.com>
2021-04-29 17:08:11 -0700

    trying the git key setting

--- scripts/auth.sh
+++ scripts/auth.sh
@@ -3,6 +3,7 @@ source $ACTION_WORKSPACE/keg-cli/keg
 
 set_docker_user () {
   keg config set key=docker.user value=$INPUT_USER --confirm false
+  keg config set key=cli.git.key value=$INPUT_TOKEN --confirm false
 }
 
 set_git_user () {
@@ -10,7 +11,8 @@ set_git_user () {
 }
 
 docker_login () {
-  GIT_KEY=$INPUT_TOKEN keg dp login 
+  # GIT_KEY=$INPUT_TOKEN keg dp login 
+  keg dp login 
 }
 
 set_docker_user
